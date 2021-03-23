17f5aa9464539ccb021f197da61613107b8d15aa
Riya <riyachaddha02@gmail.com>
2021-03-23 17:48:12 +0530

    ok

--- demo-project/src/hello.java
+++ demo-project/src/hello.java
@@ -1,8 +1,8 @@
 
 public class hello {
 	void method() {
-		System.out.print("method 1");
-		System.out.print("method 1");
+		System.out.print("hello 1");
+		System.out.print("hello 2");
 		System.out.print("method 1");
 		System.out.print("method 1");
 		System.out.print("method 1");
